944f90b5e60e27f6a2249619dab5d2dc11e2e3ad
Yosmangel <yadamesp@everis.com>
2020-04-01 17:35:38 +0200

    Add page

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DeseosService} from '../../services/deseos.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,11 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public deseosService: DeseosService,
+              private router: Router) {
+  }
 
+  agregarLista() {
+    this.router.navigateByUrl('/tabs/tab1/agregar');
+  }
 }
