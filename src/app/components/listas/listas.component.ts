0bad002a6455d6d22ed23df70114a6570b3bf5d6
Yosmangel <yadamesp@everis.com>
2020-04-03 17:29:36 +0200

    Editar Lista

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {DeseosService} from '../../services/deseos.service';
 import {Router} from '@angular/router';
 import {Lista} from '../../models/lista.model';
+import {AlertController, IonList} from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -9,11 +10,13 @@ import {Lista} from '../../models/lista.model';
   styleUrls: ['./listas.component.scss'],
 })
 export class ListasComponent implements OnInit {
+  // @ts-ignore
+  @ViewChild( IonList ) lista: IonList;
   @Input() terminada = true;
 
-
   constructor(public deseosService: DeseosService,
-              private router: Router) {
+              private router: Router,
+              public alertCtrl: AlertController) {
   }
 
   ngOnInit() {}
@@ -25,7 +28,43 @@ export class ListasComponent implements OnInit {
       this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
     }
   }
+
   borrar(lista: Lista) {
     this.deseosService.borrarLista(lista);
+  }
+
+  async editarLista(lista: Lista) {
+    const alert = await this.alertCtrl.create({
+      header: 'Editar Lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista',
+          value: lista.titulo
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.lista.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Guardar',
+          handler: (data) => {
+            this.lista.closeSlidingItems();
+            if (data.titulo.length === 0) {
+              return;
+            }
+            lista.titulo = data.titulo;
+            this.deseosService.guardarStorage();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
