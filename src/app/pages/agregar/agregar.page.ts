62d767580335ce221a74f38c3840030f5e73b7d4
Yosmangel <yadamesp@everis.com>
2020-04-03 15:58:19 +0200

    Eliminar items de la lista

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -32,5 +32,24 @@ export class AgregarPage implements OnInit {
     this.deseosService.guardarStorage();
   }
 
+  cambioCheck(item: ListaItem) {
+    const pendientes = this.lista.items
+        .filter( itemData => !itemData.completado)
+        .length;
 
+    if (pendientes === 0) {
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    } else {
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+    }
+
+    this.deseosService.guardarStorage();
+  }
+
+  borrar(i) {
+    this.lista.items.splice(i, 1);
+    this.deseosService.guardarStorage();
+  }
 }
